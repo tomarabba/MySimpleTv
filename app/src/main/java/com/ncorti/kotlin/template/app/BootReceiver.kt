2c7b1df64e0ca88687151312116f435988b93378
tomarabba <163319454+tomarabba@users.noreply.github.com>
2026-09-25 17:37:31 +0600

    Create BootReceiver.kt

---
+++ app/src/main/java/com/ncorti/kotlin/template/app/BootReceiver.kt
@@ -0,0 +1,16 @@
+package com.ncorti.kotlin.template.app
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            val launchIntent = Intent(context, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            }
+            context.startActivity(launchIntent)
+        }
+    }
+}
